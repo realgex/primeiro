894e1b0afbc4fbc2a0a14928c2970aba519d8ea9
realgex <realgex@gmail.com>
2016-05-17 21:50:06 -0300

    Update modafoca.c

--- modafoca.c
+++ modafoca.c
@@ -5,3 +5,9 @@ int main {
 int a,b,c;
 printf("Digite um número: ");
 scanf("%d", a);
+printf("Digite outro número: ");
+scanf("%d", b);
+c = a+b;
+printf("O resultado é: ", c);
+system("pause");
+}
